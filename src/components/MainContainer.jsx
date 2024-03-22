035fac78e95f42c2789a2bc08418a2457b4321d7
Mark Jomel Mallari <MarkJomelO.Mallari@pccw.com>
2024-03-22 20:24:59 +0800

    adding cursive font x and o player

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,25 +4,35 @@ const MainContainer = () => {
   return (
     <>
       <h1 className="m-4">TicTacToe Game</h1>
-      <div class="flex h-[600px] w-[600px] p-4 border-1 bg-white my-10 text-center items-center justify-center min-w-[390px]">
-        <div class="grid grid-cols-3 border-2 border-black-100 w-full h-full">
-          <div className="bg-stone-100 p-8 border-r-2 border-b-2 border-black">
-            01
+      <div class="flex h-[600px] w-[600px] p-4 border-1 bg-white text-center min-w-[400px]">
+        <div class="grid grid-cols-3 w-full">
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4 border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
           </div>
-          <div className="bg-stone-100 p-8 border-r-2 border-b-2 border-black">
-            02
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4 border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">O</span>
           </div>
-          <div className="bg-stone-100 p-8 border-b-2 border-black">03</div>
-          <div className="bg-stone-100 p-8 border-r-2 border-b-2 border-black">
-            04
+          <div className="flex justify-center items-center bg-stone-100 border-black border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
           </div>
-          <div className="bg-stone-100 p-8 border-r-2 border-b-2 border-black">
-            05
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4 border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
           </div>
-          <div className="bg-stone-100 p-8  border-b-2 border-black">06</div>
-          <div className="bg-stone-100 p-8 border-r-2 border-black">07</div>
-          <div className="bg-stone-100 p-8 border-r-2  border-black">08</div>
-          <div className="bg-stone-100 p-8  border-black">09</div>
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4 border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">O</span>
+          </div>
+          <div className="flex justify-center items-center bg-stone-100 border-black border-b-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
+          </div>
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
+          </div>
+          <div className="flex justify-center items-center bg-stone-100 border-black border-r-4">
+            <span className="leading-10 text-[9.25rem] font-cursive">O</span>
+          </div>
+          <div className="flex justify-center items-center bg-stone-100 border-black">
+            <span className="leading-10 text-[9.25rem] font-cursive">X</span>
+          </div>
         </div>
       </div>
     </>
